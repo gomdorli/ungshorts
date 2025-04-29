a129fb1ec10a271820a844b1d5900b430bf14a4a
gomdorli <bosskim185@naver.com>
2025-04-29 23:39:34 +0900

    Rename local telegram package to bot to avoid name collision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from scheduler import start_scheduler
-from telegram.telegram_bot import start_telegram_bot
+from bot.telegram_bot import start_telegram_bot
 from utils.logger import setup_logger
 from uploader.sheets_logger import setup_sheets
 from stats.stats_manager import monitor_video_stats, send_weekly_report
